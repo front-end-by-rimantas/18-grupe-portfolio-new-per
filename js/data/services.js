36979fc585c4f848fae3210945e3d7d29837766a
karpizz <tyrant233@gmail.com>
2020-10-14 13:06:14 +0300

    my service js not final

---
+++ js/data/services.js
@@ -0,0 +1,49 @@
+const servicesData = {
+    selector: '#services_block',
+    services: [
+        {
+            id: 1,
+            icon: 'zmdi-flower-alt',
+            title: 'BRANDING',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        },
+        {
+            id:2,
+            icon: 'zmdi-devices',
+            title: 'WEB DESIGN',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        },
+        {
+            id:3,
+            icon: 'zmdi-shopping-basket',
+            title: 'MARKETING',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        },
+        {
+            id:4,
+            icon: 'zmdi-code-setting',
+            title: 'CLEAN CODE',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        },
+        {
+            id:5,
+            icon: 'zmdi-collection-item',
+            title: 'FRINDELY ALL SCREEN',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        },
+        {
+            id:6,
+            icon: 'zmdi-compass',
+            title: 'UNLIMITED SUPPORT',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.',
+            active: true
+        }
+    ]
+}
+
+export { servicesData }
