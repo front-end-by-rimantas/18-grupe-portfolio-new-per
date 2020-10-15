1820474074fb054daa25e6bf92b055c6e4e31805
Virginija <virginijajnlt@gmail.com>
2020-10-15 09:32:52 +0200

    Update html and css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // header import
 // hero import
 // about import
-// latest blog import
-import renderBlog from './renderBlog.js';
-import blogData from './blogData.js';
+// // latest blog import
+// import renderBlog from './renderBlog.js';
+// import blogData from './blogData.js';
 
 
 // header logic
 // hero logic
 // about logic
 // latest blog logic
-renderBlog(blogData);
+// renderBlog(blogData);
