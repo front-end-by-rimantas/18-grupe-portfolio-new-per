4b2ccd3f163cedcf7f589f1a244d9fde6a20925d
karpizz <tyrant233@gmail.com>
2020-10-18 16:49:00 +0300

    commit

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,7 @@
 const track = document.querySelector('.review');
+const track1 = document.querySelector('.client-review');
 const slides = Array.from(track.children);
-// console.log(slides[0]);
+// console.log(slides[2]);
 const dotsNav = document.querySelector('.slideButtons');
 const dots = Array.from(dotsNav.children);
 
@@ -12,18 +13,11 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
-function udpateSlide() {
-    // const targetIndex = slides.findIndex(review => review === slides);
-    // const targetSlide = slides[targetIndex];
+// const move = (track, currentSlide, nextSlide) => {
+    
+// };
 
-    // console.log(targetIndex);
-    for(let slide of slides) {
-        slide.classList.remove('is-visible');
-        slide.classList.add('is-hidden');
-    // }
-    // currentSlide.classList.remove('is-visible');
-    // targetSlide.classList.add('is-visible');
-};
+// move();
 
 dotsNav.addEventListener('click', e => {
     const targetDot = e.target.closest('button');
@@ -32,6 +26,11 @@ dotsNav.addEventListener('click', e => {
     const currentDot = dotsNav.querySelector('.activeBar');
     const targetIndex = dots.findIndex(dot => dot === targetDot);
     const targetSlide = slides[targetIndex];
+    
+    const currentSlide = track.querySelector('.is-visible');
+    const nextSlide = currentSlide.nextElementSibling;
+    currentSlide.classList.remove('is-visible');
+    nextSlide.classList.add('is-visible');
 
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentDot.classList.remove('activeBar');
