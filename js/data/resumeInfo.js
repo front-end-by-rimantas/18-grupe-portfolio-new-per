01821da71079c4551d178bb5bb0974313d3750a8
zenka zenka <zenka4@inbox.lt>
2020-10-19 17:40:04 +0300

    resume info update

--- js/data/resumeInfo.js
+++ js/data/resumeInfo.js
@@ -9,13 +9,13 @@ const resumeInfo = [
         occupation: 'edu',
         line1: 'Jan 2005 - Dec 2005',
         line2: 'SPECIALIZATION COURSE 2',
-        line3: 'Stanford'
+        line3: 'Leland Stanford Junior University'
     },
     {
         occupation: 'edu',
         line1: 'Jan 2006 - Dec 2006',
         line2: 'SPECIALIZATION COURSE 3',
-        line3: 'Harvard University,Cambridge, United States'
+        line3: 'Harvard University, Cambridge, United States'
     },
     {
         occupation: 'work',
@@ -27,13 +27,13 @@ const resumeInfo = [
         occupation: 'work',
         line1: 'Jan 2008 - Dec 2008',
         line2: 'SPECIALIZATION COURSE 5',
-        line3: 'Google'
+        line3: 'Google, Los Angeles, United States'
     },
     {
         occupation: 'work',
         line1: 'Jan 2009 - Dec 2009',
         line2: 'SPECIALIZATION COURSE 6',
-        line3: 'Facebook'
+        line3: 'Facebook, Menlo Park, California'
     }
 ]
 export default resumeInfo;
