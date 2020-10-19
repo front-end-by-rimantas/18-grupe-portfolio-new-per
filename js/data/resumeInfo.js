43595209ba7a4878c9de37954b5cc39a0ab5ee0e
zenka zenka <zenka4@inbox.lt>
2020-10-19 17:24:12 +0300

    info

---
+++ js/data/resumeInfo.js
@@ -0,0 +1,39 @@
+const resumeInfo = [
+    {
+        occupation: 'edu',
+        line1: 'Jan 2004 - Dec 2004',
+        line2: 'SPECIALIZATION COURSE 1',
+        line3: 'University Of Studies, Poland, Cracow'
+    },
+    {
+        occupation: 'edu',
+        line1: 'Jan 2005 - Dec 2005',
+        line2: 'SPECIALIZATION COURSE 2',
+        line3: 'Stanford'
+    },
+    {
+        occupation: 'edu',
+        line1: 'Jan 2006 - Dec 2006',
+        line2: 'SPECIALIZATION COURSE 3',
+        line3: 'Harvard University,Cambridge, United States'
+    },
+    {
+        occupation: 'work',
+        line1: 'Jan 2007 - Dec 2007',
+        line2: 'SPECIALIZATION COURSE 4',
+        line3: 'Microsoft'
+    },
+    {
+        occupation: 'work',
+        line1: 'Jan 2008 - Dec 2008',
+        line2: 'SPECIALIZATION COURSE 5',
+        line3: 'Google'
+    },
+    {
+        occupation: 'work',
+        line1: 'Jan 2009 - Dec 2009',
+        line2: 'SPECIALIZATION COURSE 6',
+        line3: 'Facebook'
+    }
+]
+export default resumeInfo;
