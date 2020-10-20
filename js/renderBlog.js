b388442839885d8b6fb77a504e38358825c182bd
Virginija <virginijajnlt@gmail.com>
2020-10-20 14:41:09 +0200

    Update js Latest Blog

--- js/renderBlog.js
+++ js/renderBlog.js
@@ -1,13 +1,11 @@
 import blogData from './blogData.js';
 import renderSingleBlog from './renderSingleBlog.js';
-
 function renderBlog(blogData) {
     const numberBlog = blogData.length;
     for (let i = 0; i < numberBlog; i++) {
         const singleBlog = blogData[i];
         const HTML = renderSingleBlog(singleBlog);
         const selector = `.blog${singleBlog.id}`;
-        console.log(selector);
         const blogDOM = document.querySelector(`.blog${singleBlog.id}`);
         blogDOM.innerHTML = HTML;
     }
--- js/renderSingleBlog.js
+++ js/renderSingleBlog.js
@@ -1,14 +1,11 @@
 import blogAuthor from './blogAuthor.js';
 
 function renderSingleBlog(data) {
-    console.log(data);
     return `<a class='blog-link1' href='${data.linksubject}'>${data.subject}</a>
-            <img src='${data.image}' alt='Nuotrauka${data.id}'>
-            
+            <img src='${data.image}' alt='Nuotrauka${data.id}'>       
             <h4><a href='${data.linktitle}'>${data.title}</a></h4>
             <p>${data.text}</p>
             <div class='author'> ${blogAuthor(data.id, data.author)}</div>`;
-    console.log(data.image);
 }
 
 export default renderSingleBlog;
