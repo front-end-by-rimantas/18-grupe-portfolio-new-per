21a60ba27d1536624df9a5cf9100e42f75660cda
karpizz <tyrant233@gmail.com>
2020-10-27 10:34:10 +0200

    commit

--- js/data/counter.js
+++ js/data/counter.js
@@ -7,21 +7,21 @@ const dataCounter = {
             number: '38',
             text: 'happy clients'
         },
-        // {
-        //     icon: 'fa fa-check-square-o',
-        //     number: '55',
-        //     text: 'successful projects'
-        // },
-        // {
-        //     icon: 'fa fa-trophy',
-        //     number: '318',
-        //     text: 'awards received'
-        // },
-        // {
-        //     icon: 'fa fa-coffee',
-        //     number: '738',
-        //     text: 'cups of tea'
-        // }
+        {
+            icon: 'fa fa-check-square-o',
+            number: '55',
+            text: 'successful projects'
+        },
+        {
+            icon: 'fa fa-trophy',
+            number: '318',
+            text: 'awards received'
+        },
+        {
+            icon: 'fa fa-coffee',
+            number: '738',
+            text: 'cups of tea'
+        }
     ]
 }
 
