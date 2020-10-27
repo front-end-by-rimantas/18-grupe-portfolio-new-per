6552175832491fbf868dc4a0ee77661e91600db9
karpizz <tyrant233@gmail.com>
2020-10-27 10:33:35 +0200

    neveikia achBottomY

--- js/components/counter/Counter.js
+++ js/components/counter/Counter.js
@@ -1,9 +1,12 @@
 class Counter {
     constructor(params) {
         this.selector = params.selector;
+        this.insertionPosition = params.insertionPosition;
         this.data = params.data;
 
         this.DOM = null;
+        this.counterDOM = null;
+        this.countersValueDOM = null;
 
         this.init();
     }
@@ -13,6 +16,8 @@ class Counter {
             return;
         }
         this.render();
+        this.addEvents();
+        this.countAnimation();
     }
 
     isValidSelector() {
@@ -27,32 +32,57 @@ class Counter {
     generateHTML() {
         let HTML = '';
         for (let item of this.data) {
-            HTML += `<div class="box col-3 col-md-6 col-sm-12">
-                        <div class="item">
-                            <i class="${item.icon}"></i>
-                            <div class="numbers">${item.number}</div>
-                            <p>${item.text}</p>
-                        </div>
+            HTML += `<div class="counterItem col-3 col-md-6 col-sm-12">
+                        <i class="${item.icon}"></i>
+                        <div class="number">0</div>
+                        <p>${item.text}</p>
                     </div>`;
         }
 
         return HTML;
     }
-
+    
     render() {
-        this.DOM.innerHTML = this.generateHTML();
+        this.DOM.insertAdjacentHTML(this.insertionPosition, this.generateHTML());
+        this.counterDOM = this.DOM.querySelectorAll('.counterItem');
+        this.countersValueDOM = this.DOM.querySelectorAll('.counterItem > .number');
+    }
+    
+    countAnimation() {
+        const screenBottomY = scrollY + innerHeight;
+        for (let i = 0; i < this.data.length; i++) {
+            const ach = this.counterDOM[i];
+            if(ach.dataset.run){
+                continue;
+            }
+            const achBottomY = ach.offsetTop + ach.offsetHeight;
+            if (screenBottomY > achBottomY) {
+                ach.dataset.run = true;
+                
+                let currentValue = 0;
+                const final = this.data[i].number;
+                const timeAmount = 3; //sekundes
+                const fps = 24; 
+                let step = 0;
+                const totalStep = timeAmount * fps;
+                
+                const timer = setInterval(() => {
+                    step++
+                    currentValue = Math.ceil(final / totalStep * step);
+                    this.countersValueDOM[i].innerText = currentValue;
+                   
+                    if(currentValue >= final){
+                        clearInterval(timer);
+                    }
+                }, 1000 / fps)
+            }  
+        }
+    }
 
-        // $('.number').each(function () {
-        //     $(this).prop('Counter', 0).animate({
-        //         Counter: $(this).text()
-        //     }, {
-        //         duration: 4000,
-        //         easing: 'swing',
-        //         step: function (now) {
-        //             $(this).text(Math.ceil(now));
-        //         }
-        //     });
-        // });
+    addEvents() {
+        addEventListener('scroll', () => {
+            this.countAnimation();
+        })
     }
 }
 
